d92ece98a7e47a76c903bebab11d247555025ca7
Mark <lookandlearn76@yahoo.co.uk>
2017-08-28 22:09:18 +0100

    added styles

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -26,3 +26,11 @@ it('should render one Todo component for each item', () => {
 
   expect(todosComponents.length).toBe(todos.length);
 });
+
+it('should render empty message if no todos', () => {
+  var todos = [];
+  var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+  var $el = $(ReactDOM.findDOMNode(todoList));
+
+  expect($el.find('.container__message').length).toBe(1);
+});
