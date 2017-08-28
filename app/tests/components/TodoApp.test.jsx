3e35057e64b06ed7c89474b3a441e0c7e8019052
Mark <lookandlearn76@yahoo.co.uk>
2017-08-28 01:27:09 +0100

    added completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,6 +19,8 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    // Expect createdAt to be a number
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
@@ -36,5 +38,27 @@ describe('TodoApp', () => {
     todoApp.handleToggle(11);
     //verify that value changed
     expect(todoApp.state.todos[0].completed).toBe(true);
+    //Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  //test that when toggle from true to false completedat get removed
+  it('should toggle todo from completed to incompleted', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: true
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    //check that todos first item has completed value of false
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    //call handleToggle with 11
+    todoApp.handleToggle(11);
+    //verify that value changed
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    //Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
